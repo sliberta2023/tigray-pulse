ca337f37c409f3c92aad6846d9281c321bcc7f14
Solomon Liberta <solomon.liberta@postclick.com>
2022-12-09 22:18:13 -0600

    created nx workspace

---
+++ apps/api/twitter-client-service/jest.config.ts
@@ -0,0 +1,16 @@
+/* eslint-disable */
+export default {
+  displayName: 'twitter-client-service',
+  preset: '../../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../../coverage/apps/api/twitter-client-service',
+};
