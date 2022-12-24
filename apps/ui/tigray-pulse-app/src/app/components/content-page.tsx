81f3c88091c96ca14d5c1b8f2b6a33468fb723f7
Solomon Liberta <solomon.liberta@postclick.com>
2022-12-24 07:41:20 -0600

    adding Card to show list of tweets

--- apps/ui/tigray-pulse-app/src/app/components/content-page.tsx
+++ apps/ui/tigray-pulse-app/src/app/components/content-page.tsx
@@ -1,7 +1,43 @@
+import Card, { CardData } from "./card";
+
+const tweets: CardData[] = [
+    {
+        title: 'Noteworthy technology acquisitions 2021',
+        message: 'Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.',
+        imgSrc: '/assets/image.jpeg',
+        imgAlt: 'sample image from the internet',
+        linkTitle: 'go to the source',
+        linkAction: (e) => console.log('Go to the source to read the details...')
+    },
+    {
+        title: 'Second noteworthy technology acquisitions 2021',
+        message: 'Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.',
+        imgSrc: '/assets/image.jpeg',
+        imgAlt: 'sample image from the internet',
+        linkTitle: 'go to the source',
+        linkAction: (e) => console.log('Go to the source to read the details...')
+    },
+    {
+        title: 'Third noteworthy technology acquisitions 2021',
+        message: 'Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.',
+        imgSrc: '/assets/image.jpeg',
+        imgAlt: 'sample image from the internet',
+        linkTitle: 'go to the source',
+        linkAction: (e) => console.log('Go to the source to read the details...')
+    }
+];
 function ContentPage() {
     return (
-        <div>
-            <p> This is the main page </p>
+        <div className='flex'>
+            {
+                tweets.map((tweet, index) => {
+                    return (
+                        <div key={`${tweet.title}_${index}`} className='m-5'>
+                            <Card>{tweet}</Card>
+                        </div>
+                    )
+                })
+            }
         </div>
     );
 }
--- apps/ui/tigray-pulse-app/src/app/components/main-page.tsx
+++ apps/ui/tigray-pulse-app/src/app/components/main-page.tsx
@@ -4,11 +4,10 @@ type props = {
   
   function MainPage({ children }: props) {
     return (
-      <div className="select-none z-10 min-h-[100vh] flex flex-col justify-between w-[90%] md:w-[670px] mx-auto px-1">
+      <div className="select-none z-10 min-h-[100vh] flex flex-col justify-between mx-auto px-1">
         {children}
       </div>
     );
   }
   
   export default MainPage;
-  
